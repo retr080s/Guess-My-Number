ac4fe732d9734c8d9f7251dd587f341b4370721e
retr080s <retr080s@protonmail.com>
2022-10-24 00:42:11 +0200

    code refactoring

--- script.js
+++ script.js
@@ -40,10 +40,10 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.highscore').textContent = highscore;
         }
 
-        // When guess is too high
-    } else if (guess > secretNumber) {
+        // When guess is wrong
+    } else if (guess !== secretNumber) {
         if (score > 1) {
-            document.querySelector('.message').textContent = '📈 Too high!';
+            document.querySelector('.message').textContent = guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
             score--;
             document.querySelector('.score').textContent = score;
         } else {
@@ -51,16 +51,29 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.score').textContent = 0;
         }
 
-        // When guess is too low
-    } else if (guess < secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = '📉 Too low!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = '💥 You lost the game!';
-            document.querySelector('.score').textContent = 0;
-        }
+
+        //     // When guess is too high
+        // } else if (guess > secretNumber) {
+        //     if (score > 1) {
+        //         document.querySelector('.message').textContent = '📈 Too high!';
+        //         score--;
+        //         document.querySelector('.score').textContent = score;
+        //     } else {
+        //         document.querySelector('.message').textContent = '💥 You lost the game!';
+        //         document.querySelector('.score').textContent = 0;
+        //     }
+
+        //     // When guess is too low
+        // } else if (guess < secretNumber) {
+        //     if (score > 1) {
+        //         document.querySelector('.message').textContent = '📉 Too low!';
+        //         score--;
+        //         document.querySelector('.score').textContent = score;
+        //     } else {
+        //         document.querySelector('.message').textContent = '💥 You lost the game!';
+        //         document.querySelector('.score').textContent = 0;
+        //     }
+        // }
     }
 });
 
